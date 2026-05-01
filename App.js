510c4c58ea8f2b2cf8fc831d77f3cc83255cba40
Zion <amazion24@outlook.com>
2026-05-01 09:05:47 +0300

    MySafe

--- App.js
+++ App.js
@@ -3,12 +3,14 @@ import { StyleSheet, Text, View } from 'react-native';
 import Ramzor from './components/Ramzor'
 import MyGrid from './components/MyGrid';
 import MyView2 from './components/MyView2';
+import MySafe from './components/MySafe';
 
 export default function App() {
   return (
     // <Ramzor/>
     // <MyGrid/>
-    <MyView2/>
+    // <MyView2/>
+    <MySafe/>
   );
 }
 
