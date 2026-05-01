98c2e9fcddf29520e4ac1425bac69f35148ca276
Zion <amazion24@outlook.com>
2026-05-01 09:19:53 +0300

    MyStatus

--- App.js
+++ App.js
@@ -4,13 +4,15 @@ import Ramzor from './components/Ramzor'
 import MyGrid from './components/MyGrid';
 import MyView2 from './components/MyView2';
 import MySafe from './components/MySafe';
+import MyStatus from './components/MyStatus';
 
 export default function App() {
   return (
     // <Ramzor/>
     // <MyGrid/>
     // <MyView2/>
-    <MySafe/>
+    // <MySafe/>
+    <MyStatus/>
   );
 }
 
---
+++ components/MyStatus.jsx
@@ -0,0 +1,15 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React, { useState } from 'react'
+import { StatusBar } from 'expo-status-bar'
+
+export default function MyStatus() {
+    let [isLight, setIsLight] = useState(true);
+    return (
+        <View>
+            <StatusBar style={isLight?'dark':'light'} />
+            <Text>MyStatus</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({})
